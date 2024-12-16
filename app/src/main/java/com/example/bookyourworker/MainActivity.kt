406dd383e55eae64251f3cb54567b4dad6e7e4a5
Dinkar Arya <dinkararya09@gmail.com>
2024-12-16 09:42:17 +0530

    Button added

--- app/src/main/java/com/example/bookyourworker/MainActivity.kt
+++ app/src/main/java/com/example/bookyourworker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.bookyourworker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
